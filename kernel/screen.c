93a4b57e3da50b448e7804a649eaaa8fc7815f96
Mobeen Ahmed <amobeen652@gmail.com>
2026-04-21 18:32:35 +0500

    making screen pixels login

---
+++ kernel/screen.c
@@ -0,0 +1,22 @@
+unsigned short *vga_buffer = (unsigned short *)0xB8000;
+
+const int Row = 25;
+const int Col = 80;
+
+void clear_screen()
+{
+    for (int i = 0; i < Row * Col; i++)
+    {
+        vga_buffer[i] = (unsigned short)' ' | 0x0400;
+    }
+}
+
+void print(const char *str, int row, int col)
+{
+    int ind = (Row * Col) + col;
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        vga_buffer[ind + i] = (unsigned short)str[i] | 0x0400;
+    }
+}
