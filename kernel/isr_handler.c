2344d0c32a65dbdb59287ce83213be3ad997d389
Mobeen Ahmed <amobeen652@gmail.com>
2026-04-24 16:57:54 +0500

    Add ISR handler and register structure

---
+++ kernel/isr_handler.c
@@ -0,0 +1,13 @@
+#include "isr.h"
+
+void isr_handler(struct registers r){
+    if(r.int_no==33){
+       keyboard_handler();
+    }else if(r.int_no<32) {
+        print_string("CPU HANDLES IT : ");
+        print_hex(r.int_no);
+        asm volatile("hlt");
+    }
+
+    if(r.int_no>32) outb(0x20,0x20);
+}
--- kernel/stack_guard.c
+++ kernel/stack_guard.c
@@ -5,7 +5,7 @@ uintptr_t __stack_chk_guard = 0xDEADC0DE;
 __attribute__((noreturn)) void __stack_chk_fail()
 {
 
-    print_string("PANIC MODE : STACK SMASHING DETECT! GO LOOK AT IT ");
+    print_string("PANIC MODE : STACK SMASHING DETECT! GO LOOK AT IT");
     
     while (1)
     {
