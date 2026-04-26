35a5b1e0f52777dd72fbe98dc6b36f2fbe08a036
Mobeen Ahmed <amobeen652@gmail.com>
2026-04-26 16:15:03 +0500

    Moving from circular buffer terminal hack to real terminal shit

--- kernel/terminal.c
+++ kernel/terminal.c
@@ -1,21 +1,26 @@
-#define VGA_WIDTH 80
-#define VGA_HEIGHT 25
-#define TOTAL_LINES 200
+#define WIDTH 80
+#define HEIGHT 25
+#define MAX_LINES 200
 
-uint16_t *const vga_memory = (uint16_t *)0xB8000;
+uint8_t current_color=0x0F;
+
+char buffer[MAX_LINES][WIDTH];
+uint8_t colors[MAX_LINES][WIDTH];
+
+uint16_t *const vga_memory=(uint16_t*)0xB8000;
 
 int cursor_x = 0;   // Columns
 int cursor_y = 0;   // Logical line
 int scroll_top = 0; // first line
 
-void clear_line(int line)
-{
-    int start = line * VGA_WIDTH;
-    for (int i = 0; i < VGA_WIDTH; i++)
-    {
-        vga_memory[start + i] = ' ' | (current_color << 8);
-    }
-}
+// void clear_line(int line)
+// {
+//     int start = line * WIDTH;
+//     for (int i = 0; i < WIDTH; i++)
+//     {
+//         vga_memory[start + i] = ' ' | (current_color << 8);
+//     }
+// }
 
 void terminal_writeData(char c)
 {
@@ -23,17 +28,34 @@ void terminal_writeData(char c)
     {
         cursor_x = 0;
         cursor_y++;
+        return;
     }
-    int physical_line = cursor_y % TOTAL_LINES;
-    int physical_index = VGA_WIDTH * physical_line + cursor_x; // line->Flat Memory
+    buffer[cursor_y][cursor_x]=c;
+    colors[cursor_y][cursor_x]=current_color;
+    
 
-    vga_memory[physical_index] = (uint16_t)c | (current_color << 8);
-    cursor_x++;
-
-    if (cursor_x >= VGA_WIDTH)
+    if (cursor_x >= WIDTH)
     {
         cursor_x = 0;
         cursor_y++;
     }
-    clear_line(cursor_y % TOTAL_LINES);
+
+    if (cursor_y >= scroll_top + HEIGHT)   scroll_top++;
+
+
+}
+
+void render(){
+    for (int y=0;y<HEIGHT;y++){
+        int line = scroll_top+y;
+
+        for (int x = 0; x < WIDTH; x++)
+        {
+            char c=buffer[line][x];
+            uint8_t color=colors[line][x];
+
+            vga_memory[y*WIDTH+x]=(uint16_t)c | (color << 8 );
+        }
+        
+    }
 }
