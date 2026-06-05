8e3c38fd6bd0f1d33300ce8bd1d0c37a37d4ad92
Mobeen Ahmed <amobeen652@gmail.com>
2026-06-05 17:39:04 +0500

    Improve timer schedule tell them which register to check

--- kernel/Process/timer_handler.asm
+++ kernel/Process/timer_handler.asm
@@ -5,7 +5,13 @@ extern schedule
 timer_handler_interrupt:
     pusha
 
+    push esp ; Save the current stack pointer for the scheduler
     call schedule
+    add esp, 4 ; Clean up the stack after the call to schedule
 
+    mov al, 0x20 ; Send End of Interrupt (EOI) signal to the PIC
+    out 0x20, al
+
+    
     popa 
     iret
